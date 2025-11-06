4cd546889657d51fecfd41430afd670f4b8c4287
Basedfloppa <bababooooooye@gmail.com>
2025-11-06 21:21:11 +0300

    stupid perf fixes for affinity

--- parser-api/src/utils.rs
+++ parser-api/src/utils.rs
@@ -1,6 +1,6 @@
 use chrono::{DateTime, Utc};
 use std::collections::HashMap;
-use rocket::serde::Deserialize;
+use serde::Deserialize;
 use crate::models::{Post, TagCount};
 
 #[derive(Debug, Clone, Deserialize)]
@@ -19,86 +19,72 @@ fn sigmoid(x: f32) -> f32 {
     1.0 / (1.0 + (-x).exp())
 }
 
+#[inline]
+fn gw<'a>(group_wts: &HashMap<&'a str, f32>, group: &'a str) -> f32 {
+    *group_wts.get(group).unwrap_or(&1.0)
+}
+
 pub fn post_affinity(
     account_tag_counts: &[TagCount],
     origin_post: &Post,
     group_wts: &HashMap<String, f32>,
     priors: &Priors,
 ) -> f32 {
-    let mut post_tags: Vec<(String, String)> = Vec::new();
+    let group_wts_hash: HashMap<&str, f32> = group_wts.iter().map(|(k, v)| (k.as_str(), *v)).collect();
+    let mut post: HashMap<(&str, &str), f32> = HashMap::default();
 
-    post_tags.extend(origin_post.clone().tags.artist.into_iter().map(|t| (t, "artist".into())));
-    post_tags.extend(origin_post.clone().tags.character.into_iter().map(|t| (t, "character".into())));
-    post_tags.extend(origin_post.clone().tags.copyright.into_iter().map(|t| (t, "copyright".into())));
-    post_tags.extend(origin_post.clone().tags.general.into_iter().map(|t| (t, "general".into())));
-    post_tags.extend(origin_post.clone().tags.lore.into_iter().map(|t| (t, "lore".into())));
-    post_tags.extend(origin_post.clone().tags.meta.into_iter().map(|t| (t, "meta".into())));
-    post_tags.extend(origin_post.clone().tags.species.into_iter().map(|t| (t, "species".into())));
+    for t in &origin_post.tags.artist     { post.entry((t.as_str(), "artist"))    .or_insert(gw(&group_wts_hash, "artist")); }
+    for t in &origin_post.tags.character  { post.entry((t.as_str(), "character")) .or_insert(gw(&group_wts_hash, "character")); }
+    for t in &origin_post.tags.copyright  { post.entry((t.as_str(), "copyright")) .or_insert(gw(&group_wts_hash, "copyright")); }
+    for t in &origin_post.tags.general    { post.entry((t.as_str(), "general"))   .or_insert(gw(&group_wts_hash, "general")); }
+    for t in &origin_post.tags.lore       { post.entry((t.as_str(), "lore"))      .or_insert(gw(&group_wts_hash, "lore")); }
+    for t in &origin_post.tags.meta       { post.entry((t.as_str(), "meta"))      .or_insert(gw(&group_wts_hash, "meta")); }
+    for t in &origin_post.tags.species    { post.entry((t.as_str(), "species"))   .or_insert(gw(&group_wts_hash, "species")); }
 
-    if account_tag_counts.is_empty() || post_tags.is_empty() {
-        return 0.0;
-    }
-
-    let mut user: HashMap<(String, String), f32> = HashMap::with_capacity(account_tag_counts.len());
+    let mut user: HashMap<(&str, &str), f32> = HashMap::default();
     for t in account_tag_counts {
-        if t.count <= 0 {
-            continue;
-        }
-
-        let gw = *group_wts.get(&t.group_type).unwrap_or(&1.0);
-
-        let w = (t.count as f32).ln_1p() * gw;
+        if t.count <= 0 { continue; }
+        let w = (t.count as f32).ln_1p() * *group_wts_hash.get(t.group_type.as_str()).unwrap_or(&1.0);
         if w > 0.0 {
-            *user.entry((t.name.clone(), t.group_type.clone())).or_insert(0.0) += w;
+            *user.entry((t.name.as_str(), t.group_type.as_str())).or_insert(0.0) += w;
         }
     }
-    if user.is_empty() {
-        return 0.0;
-    }
 
-    let mut post: HashMap<(String, String), f32> = HashMap::with_capacity(post_tags.len());
-    for (name, group) in post_tags {
-        let gw = *group_wts.get(group.as_str()).unwrap_or(&1.0);
-        post.entry((name, group)).or_insert(gw);
-    }
-    if post.is_empty() {
-        return 0.0;
-    }
+    let mut dot = 0.0f32;
+    let mut u_norm_sq = 0.0f32;
+    let mut p_norm_sq = 0.0f32;
 
-    let mut dot: f32 = 0.0;
-    let mut u_norm_sq: f32 = 0.0;
-    let mut p_norm_sq: f32 = 0.0;
+    for &uw in user.values()  { u_norm_sq += uw * uw; }
+    for &pw in post.values()  { p_norm_sq += pw * pw; }
 
-    for (_k, uw) in &user {
-        u_norm_sq += uw * uw;
-    }
-    for (_k, pw) in &post {
-        p_norm_sq += pw * pw;
-    }
-
-    let (smaller, other) = if user.len() <= post.len() {
-        (user, post)
+    if user.len() <= post.len() {
+        for (k, &uw) in &user {
+            if let Some(&pw) = post.get(k) { dot += uw * pw; }
+        }
     } else {
-        (post, user)
-    };
-    for (k, w) in smaller {
-        if let Some(&w2) = other.get(&k) {
-            dot += w * w2;
+        for (k, &pw) in &post {
+            if let Some(&uw) = user.get(k) { dot += uw * pw; }
         }
     }
 
     let sim = if u_norm_sq == 0.0 || p_norm_sq == 0.0 {
         0.0
     } else {
-        (dot / (u_norm_sq.sqrt() * p_norm_sq.sqrt())).clamp(0.0, 1.0)
+        dot / (u_norm_sq.sqrt() * p_norm_sq.sqrt())
     };
 
-    let quality =
-        sigmoid(priors.quality_a * (origin_post.score.total as f32) + priors.quality_b * (origin_post.fav_count as f32));
-    let age_days = (priors.now - origin_post.created_at).num_seconds() as f32 / 86400.0;
-    let recency = (-age_days / priors.recency_tau_days.max(1e-3))
-        .exp()
-        .clamp(0.0, 1.0);
+    let quality = sigmoid(
+        priors.quality_a * origin_post.score.total as f32 +
+            priors.quality_b * origin_post.fav_count as f32
+    );
 
-    (priors.mix_sim * sim + priors.mix_quality * quality + priors.mix_recency * recency).clamp(0.0, 1.0)
+    let age_days = (priors.now - origin_post.created_at).num_seconds() as f32 / 86_400.0;
+    let recency = (-age_days / priors.recency_tau_days.max(1e-3)).exp().clamp(0.0, 1.0);
+
+    let sum = priors.mix_sim + priors.mix_quality + priors.mix_recency;
+    let (ms, mq, mr) = if sum > 0.0 {
+        (priors.mix_sim / sum, priors.mix_quality / sum, priors.mix_recency / sum)
+    } else { (0.0, 0.0, 0.0) };
+
+    (ms * sim + mq * quality + mr * recency).clamp(0.0, 1.0)
 }
